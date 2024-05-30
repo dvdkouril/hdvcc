9d089415f3c13c54fdc5f2fdcc2055629f36df47
David Kouril <david.kouril@gmail.com>
2024-05-30 15:14:53 -0400

    feat: vector3 methods

--- raytracing/src/main.zig
+++ raytracing/src/main.zig
@@ -12,7 +12,67 @@ const Vec3 = struct {
     pub fn init(x: f32, y: f32, z: f32) Vec3 {
         return Vec3{ .x = x, .y = y, .z = z };
     }
+
+    pub fn dot(self: Vec3, other: Vec3) f32 {
+        return self.x * other.x + self.y * other.y + self.z * other.z;
+    }
+
+    pub fn add(self: Vec3, other: Vec3) Vec3 {
+       return Vec3.init(self.x + other.x,
+                        self.y + other.y,
+                        self.z + other.z);
+    }
+
+    pub fn inverse(self: Vec3) Vec3 {
+        return Vec3.init(-self.x, -self.y, -self.z);
+    }
+
+    pub fn scaled(self: Vec3, s: f32) Vec3 {
+        return Vec3.init(self.x * s, self.y * s, self.z * s);
+    }
+
+    pub fn length(self: Vec3) f32 {
+        return std.math.sqrt(self.lengthSquared());
+    }
+
+    pub fn lengthSquared(self: Vec3) f32 {
+        return Vec3.dot(self, self);
+    }
+
+    // inline vec3 cross(const vec3& u, const vec3& v) {
+    //     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+    //                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
+    //                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+    // }
+
+    // inline vec3 unit_vector(const vec3& v) {
+    //     return v / v.length();
+    // }
+
+    pub fn cross(self: Vec3, other: Vec3) Vec3 {
+        return Vec3(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        );
+    }
+
+    pub fn normalized(self: Vec3) Vec3 {
+        return Vec3.scaled(self, 1 / self.length());
+    }
+
+    pub fn format(
+        self: Vec3,
+        comptime _: []const u8,
+        _: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        try writer.print("Vec3({}, {}, {})\n", .{self.x, self.y, self.z });
+    }
+
 };
+
+const Point3 = Vec3;
 
 pub fn main() !void {
     const image_width = 256;
@@ -44,7 +104,14 @@ pub fn main() !void {
     }
 
     std.debug.print("\rDone.    \n", .{});
+
+
     try bw.flush(); // don't forget to flush!
+
+    const posA = Point3.init(3, 2, 1);
+    const posB = Point3.init(1, 2, 3);
+    const res = posA.add(posB);
+    std.debug.print("res = {}", .{ res });
 }
 
 test "simple test" {
@@ -52,4 +119,21 @@ test "simple test" {
     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "Vec3.add" {
+    const a = Vec3.init(3.0, 2.0, 1.0);
+    const b = Vec3.init(1.0, 2.0, 3.0);
+    const result = a.add(b);
+
+    try std.testing.expect(@as(usize, @intFromFloat(result.x)) == 4);
+    try std.testing.expect(@as(usize, @intFromFloat(result.y)) == 4);
+    try std.testing.expect(@as(usize, @intFromFloat(result.z)) == 4);
+}
+
+test "Vec3.normalized" {
+    const vec = Vec3.init(5000, 42, 123);
+    const nlength = @round(vec.normalized().length());
+    // std.debug.print("{}\n {}", .{ vec, nlength });
+    try std.testing.expectEqual(1, @as(usize, @intFromFloat(nlength)));
 }
